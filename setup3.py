63ee7d3d216e6457db67944a230618647f4b59b6
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2023-10-07 19:16:18 -0400

    Update setup3.py

--- setup3.py
+++ setup3.py
@@ -14,8 +14,7 @@ stdin, stdout, stderr = client.exec_command('''
                                             rm -rf c4_deployment-5
                                             git clone https://github.com/kura-labs-org/c4_deployment-5.git
                                             cd c4_deployment-5
-                                            python -m gunicorn app:app -b 0.0.0.0 -D
-                                            echo "Done"
+                                            python -m gunicorn app:app -b 0.0.0.0 -D && echo "Done"
                                             ''')
 
 print(stdout.read().decode("utf-8"))
