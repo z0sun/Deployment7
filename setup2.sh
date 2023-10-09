e271d6f79182c613d0b2d06a4a6edf03b84fbb73
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2023-10-09 10:00:00 -0400

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-cd c4_deployment-5
-chmod 700 Pkill.sh
-./Pkill.sh && echo "Done"
+#!/bin/bash
+if [[ $(ps aux | grep -i "gunicorn" | tr -s " " | head -n 1 | cut -d " " -f 2) != 0 ]]
+then
+ps aux | grep -i "gunicorn" | tr -s " " | head -n 1 | cut -d " " -f 2 > pid.txt
+kill $(cat pid.txt)
+fi
+
+exit 0
