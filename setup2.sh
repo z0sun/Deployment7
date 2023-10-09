c0b52e7d17680a527050db47f5cd6699c1161798
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2023-10-09 16:04:48 -0400

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -3,4 +3,8 @@ source test/bin/activate
 rm -rf c4_deployment-5
 git clone https://github.com/kura-labs-org/c4_deployment-5.git
 cd c4_deployment-5
+python database.py
+sleep 1
+python load_data.py
+sleep 1 
 python -m gunicorn app:app -b 0.0.0.0 -D && echo "Done"
