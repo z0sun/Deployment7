21d40f7ba14479dad72dd15894ca072847e88afe
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2023-10-10 13:45:41 -0400

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-source test/bin/activate
 rm -rf c4_deployment-5
+source test/bin/activate
 git clone https://github.com/kura-labs-org/c4_deployment-5.git
 cd c4_deployment-5
+pip install -r requirements.txt
+pip install gunicorn
 python database.py
 sleep 1
 python load_data.py
