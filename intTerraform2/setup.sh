1d6117fff7670e081062adc3306d5d30756fd28d
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2023-10-18 21:15:23 -0400

    Update setup.sh

--- intTerraform2/setup.sh
+++ intTerraform2/setup.sh
@@ -7,7 +7,7 @@ sudo apt install -y python3.7
 sudo apt install -y python3.7-venv
 sudo apt-get install -y build-essential
 sudo apt-get install -y libmysqlclient-dev
-sudo apt-get install -y python3-dev
+sudo apt-get install python3.7-dev
 python3.7 -m venv test
 source test/bin/activate
 git clone https://github.com/kura-labs-org/c4_deployment-5.git
