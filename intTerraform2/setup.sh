12f49463500f7139dcd2576f8b14f65af7ba5b57
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2023-10-23 18:27:39 -0400

    Update setup.sh

--- intTerraform2/setup.sh
+++ intTerraform2/setup.sh
@@ -15,10 +15,6 @@ cd c4_deployment-5
 pip install mysqlclient
 pip install -r requirements.txt
 pip install gunicorn
-python database.py
-sleep 1
-python load_data.py
-sleep 1 
 python -m gunicorn app:app -b 0.0.0.0 -D
 echo "Done"
 
